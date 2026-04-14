b4cdab1f617a8c834f677943975c2f3021cfb6d0
DANILOsOROKA <danilsorok55@gmail.com>
2026-04-14 18:18:44 +0100

    Delete Hometask2/Hometask_for_checking2_js_in_ts_module10 directory

--- Hometask2/Hometask_for_checking2_js_in_ts_module10/Hometask_10_1/HW_10_1.ts
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('button') as HTMLButtonElement;
-
-button.onclick = (): void => {
-    document.getElementById('text')?.remove();
-}
--- Hometask2/Hometask_for_checking2_js_in_ts_module10/Hometask_10_4/HW_10_4.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let a = 1;
-let count = document.getElementsByTagName("div")[0];
-let saved_count = localStorage.getItem("my_count");
-let currentCount = saved_count ? parseInt(saved_count) + 1 : a;
-if (count) {
-    count.innerText = currentCount.toString();
-    localStorage.setItem('my_count', currentCount.toString());
-}
